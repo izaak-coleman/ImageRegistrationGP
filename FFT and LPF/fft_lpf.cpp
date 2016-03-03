aa04196faa9e001c92c5f17aa075244f507d4d34
Jukka Soikkeli <jes15@ic.ac.uk>
2016-03-03 22:07:29 +0000

    Added some GPU code to FFT and LPF, to be tested properly

--- FFT and LPF/fft_lpf.cpp
+++ FFT and LPF/fft_lpf.cpp
@@ -4,9 +4,10 @@
 //GPU based:
 //http://answers.opencv.org/question/11485/opencv-gpudft-distorted-image-after-inverse-transform/
 
-
+//#include <opencv2/core/core.hpp>      // Basic OpenCV structures
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
+#include <opencv2/gpu/gpu.hpp>        // GPU structures and methods
  
 using namespace std;
 using namespace cv;
@@ -16,25 +17,69 @@ int main()
     // Read image from file
     // Make sure that the image is in grayscale
     Mat img = imread("lenna.jpg",0);
-     
+
+    //-------------- CPU version ------------------------------ 
+    /*
     Mat planes[] = {Mat_<float>(img), Mat::zeros(img.size(), CV_32F)};
     Mat complexI;    //Complex plane to contain the DFT coefficients {[0]-Real,[1]-Img}
     merge(planes, 2, complexI);
-    //dft(complexI, complexI);  // Applying DFT
-    gpu::dft(complexI, complexI, coplexI.Size,);  // Applying DFT
+    dft(complexI, complexI);  // Applying DFT
+    */
+
+    // using https://github.com/Itseez/opencv/blob/master/samples/gpu/performance/tests.cpp
+    Mat src, dst;
+    src = img;
+    dft(src,dst);
+
+
+    //-------------- GPU version ------------------------------
+    gpu::gpuMat gpuimg;
+    gpu::gpuMat gpudst;
+    gpuimg.upload(img);  //converting image mat to gpu mat
+    gpu::dft(gpuimg, gpudst, gpuimg.Size);  // Applying DFT, using real images
+    //gpu::dft(complexI, complexI, coplexI.Size,);  // Applying DFT
     //NEED GpuMat for this!
  
+
+
+
+    //------ CPU version --------
     // Reconstructing original imae from the DFT coefficients
+    /*
     Mat invDFT, invDFTcvt;
+
     idft(complexI, invDFT, DFT_SCALE | DFT_REAL_OUTPUT ); // Applying IDFT
     invDFT.convertTo(invDFTcvt, CV_8U); 
     imshow("Output", invDFTcvt);
- 
+
+
     //show the image
     imshow("Original Image", img);
-     
+
+
+    */
+
+
+    //OWN VERSION
+
+       //CPU
+       Mat out, outimg; //make matrices
+       dft(dst,out, DFT_INVERSE | DFT_REAL_OUTPUT); //getting inverse using dft function
+       out.convertTo(outimg, CV_8U);
+       //imshow("Output", outimg);
+       imwrite( "out.jpg", gray_image );
+
+       //GPU
+       gpu::gpuMat gpuout, gpuoutimg;
+       gpu::dft(dst,gpuout, DFT_INVERSE | DFT_REAL_OUTPUT);
+       gpuout.convertTo(gpuoutimg, CV_8U);
+       //imshow("Output (GPU)", gpuoutimg);
+       imwrite( "outgpu.jpg", gray_image );
+    //show the image
+       //    imshow("Original Image", img);
+
     // Wait until user press some key
-    waitKey(0);
+    //waitKey(0);
     return 0;
 }
 
