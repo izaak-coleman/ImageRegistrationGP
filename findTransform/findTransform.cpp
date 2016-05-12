7474cec878ada49014db342cd129f8c4cdcaf879
Anthony Flynn <anthonymilesflynn@gmail.com>
2016-05-12 15:22:34 +0100

    changing iterations / epsilon to appropriate values

--- findTransform/findTransform.cpp
+++ findTransform/findTransform.cpp
@@ -6,6 +6,7 @@
 // warp matrix needed to transform the input image to the same coordinates
 // as the template image, and also applies the transform to the input image
 
+#include <ctime>
 #include "opencv2/opencv.hpp"
 #include "saveWarp.h"
  
@@ -42,6 +43,8 @@ int main( int argc, char** argv )
   cv::TermCriteria criteria(cv::TermCriteria::COUNT+cv::TermCriteria::EPS,
 			number_of_iterations, termination_eps);
  
+  std::clock_t begin = clock(); // FOR TESTING PURPOSES
+
   // Run find_transformECC to find the warp matrix
   double cc = cv::findTransformECC (
 				template_image,
@@ -50,6 +53,9 @@ int main( int argc, char** argv )
 				warp_mode,
 				criteria
 				);
+
+  std::clock_t end = clock(); // FOR TESTING
+  double elapsed_secs = (end - begin) / (double) CLOCKS_PER_SEC; // FOR TESTING
  
   // Reserve a matrix to store the warped image
   cv::Mat warped_image = cv::Mat(template_image.rows, template_image.cols, CV_32FC1);
@@ -61,7 +67,10 @@ int main( int argc, char** argv )
   // Save values in the warp matrix to the filename provided on command-line
   saveWarp(outputWarpMatrix, warp_matrix);
 
-  std::cout << "Enhanced correlation coefficient between the template image and the final warped input image = " << cc << std::endl; 
+  std::cout << "Enhanced correlation coefficient between the template image "
+            << "and the final warped input image = " << cc << std::endl; 
+
+  std::cout << "Time = " << elapsed_secs << std::endl; 
 
   // Show final output
   cv::namedWindow( "Warped Image", CV_WINDOW_AUTOSIZE );
